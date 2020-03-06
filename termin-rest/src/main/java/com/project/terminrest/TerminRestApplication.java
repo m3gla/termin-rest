ddfe1ac29aa75a664144375d6bfb6dd5d6da17fe
Matija Megla <m3gla.matija@gmail.com>
2020-03-06 22:39:13 +0100

    inicijalni commit mm

---
+++ termin-rest/src/main/java/com/project/terminrest/TerminRestApplication.java
@@ -0,0 +1,13 @@
+package com.project.terminrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminRestApplication.class, args);
+	}
+
+}
---
+++ termin-rest/src/test/java/com/project/terminrest/TerminRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.terminrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
